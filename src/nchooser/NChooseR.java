9f321f58b1d2193d2678a3d8c1e596643feb6b45
fedie2562 <fedie2562@7HGM6Y1.Student.UGDSB.ED>
2018-10-26 13:31:48 -0400

    fixed for high numbers

--- src/nchooser/NChooseR.java
+++ src/nchooser/NChooseR.java
@@ -21,10 +21,10 @@ public class NChooseR {
         //
         int chosen = Integer.parseInt(JOptionPane.showInputDialog("How many objects to choose ?"));
         int objects = Integer.parseInt(JOptionPane.showInputDialog("How many objects are there to choose from ?"));
-        int answer = factorial(objects)/(factorial(chosen)*factorial(objects - chosen));
+        long answer = factorial(objects)/(factorial(chosen)*factorial(objects - chosen));
         JOptionPane.showMessageDialog(null,"There are "+answer+" ways to choose "+chosen+" from a set of "+objects+" objects.");
     }
-    public static int factorial(int n){
+    public static long factorial(int n){
         //Factorial calculation
         if((n == 0) || (n == 1)){
             return 1;
